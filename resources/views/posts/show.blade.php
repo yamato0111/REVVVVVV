d7451eba06bb2ecd0f54813a314f7ceb4852c2fe
yamato0111 <katoyama0111@icloud.com>
2023-09-11 16:52:07 +0900

    8-6

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,8 +16,11 @@
         <div class='content'>
             <div class='content_post'>
                 <h3>本文</h3>
-                <p class='body'>{{ $post->body}}</p>
+                <p class='body'>{{ $post->body }}</p>
             </div>
+        </div>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">edit</a>
         </div>
         <div class='footer'>
             <a href="/">戻る</a>
